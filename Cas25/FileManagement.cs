81cf470b195b85f4099ce0bf5cd19ed821bbb2e9
Skola Rajak QA <skolarajak.qa@gmail.com>
2020-11-25 22:00:12 +0100

    Updated program code

---
+++ Cas25/FileManagement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cas25
+{
+    class FileManagement
+    {
+        private static string FileName = @"C:\Kurs\cas25_zadatak1.txt";
+
+        public static void Write(params string[] InputData)
+        {
+            using (StreamWriter FileHandle = new StreamWriter(FileName, true))
+            {
+                FileHandle.WriteLine("{0}", String.Join(";", InputData));
+            }
+        }
+
+        public static List<string> Read()
+        {
+            List<string> ListOfNames = new List<string>();
+
+            using (StreamReader FileHandle = new StreamReader(FileName))
+            {
+                string FileContents = "";
+                while ((FileContents = FileHandle.ReadLine()) != null)
+                {
+                    ListOfNames.Add(FileContents);
+                }
+            }
+
+            return ListOfNames;
+        }
+
+    }
+}
--- Cas25/Program.cs
+++ Cas25/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
 
 namespace Cas25
 {
@@ -10,7 +9,56 @@ namespace Cas25
     {
         static void Main(string[] args)
         {
-            // Izmena
+            Zadatak1();
+            Zadatak2();
+            Console.ReadKey();
+        }
+
+        static void Zadatak1()
+        {
+            string Ime, Prezime, Email;
+
+            Console.Write("Unesite ime > ");
+            Ime = Console.ReadLine();
+
+            Console.Write("Unesite prezime > ");
+            Prezime = Console.ReadLine();
+
+            Console.Write("Unesite email > ");
+            Email = Console.ReadLine();
+
+            //FileManagement.Write(Ime, Prezime, Email);
+            // *** ili ***
+
+            string FileName = @"C:\Kurs\cas25_zadatak1.txt";
+            using (StreamWriter FileHandle = new StreamWriter(FileName, true))
+            {
+                FileHandle.WriteLine("{0};{1};{2}", Ime, Prezime, Email);
+            }
+        }
+
+        static void Zadatak2()
+        {
+            List<int> Brojevi = new List<int>();
+            string Unos;
+
+            do
+            {
+                Console.Write("Unesite broj. (Ostaviti prazno za prekid) > ");
+                Unos = Console.ReadLine();
+                if (Unos != "")
+                {
+                    Brojevi.Add(Convert.ToInt32(Unos));
+                }
+            } while (Unos != "");
+
+            foreach (int Broj in Brojevi)
+            {
+                if (Broj % 2 == 0)
+                {
+                    Console.WriteLine(Broj);
+                }
+            }
         }
     }
 }
